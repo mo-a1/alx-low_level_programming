804c7585e38f4909c897160ee9e62c48a8ed3058
Mohamed Ali <mohamed.elnakeb24@gmail.com>
2023-03-17 23:05:00 +0200

    commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -15,14 +15,14 @@ int main(void)
 	/* Print all two-digit combinations */
 	for (i = 0; i < 10; i++)
 	{
-		for (j = i + 1; j < 10; j++)
+		for (j = i; j < 10; j++)
 		{
 			putchar(i + '0');
 			putchar(',');
 			putchar(' ');
 			putchar(j + '0');
 
-			if (i == 8 && j == 9)
+			if (i == 9 && j == 9)
 			{
 				/* Last combination - don't print comma */
 				putchar('\n');
