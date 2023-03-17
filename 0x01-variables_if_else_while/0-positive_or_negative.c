66b4d046948de83957d57768a070aee8c0ac4686
Mohamed Ali <mohamed.elnakeb24@gmail.com>
2023-03-17 12:11:33 -0700

    commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,7 +19,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 		printf("%d is positive\n", n);
-	else if (n = 0)
+	else if (n == 0)
 		printf("%d is zert\n", n);
 	else
 		printf("%d is negative\n", n);
