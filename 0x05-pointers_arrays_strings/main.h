de943adadaac7a82a6754f8074b0a5a5ecbe8886
Mohamed Ali <mohamed.elnakeb24@gmail.com>
2023-03-27 22:15:00 +0200

    commit

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,24 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
+#include <stdio.h>
+
+/* Function declarations */
+void _putchar(char c);
 int _strlen(char *s);
+void _puts(char *s);
+char *_strcat(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+char *_strncpy(char *dest, char *src, int n);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+void print_number(int n);
+void print_buffer(char *b, int size);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 
 #endif /* MAIN_H */
