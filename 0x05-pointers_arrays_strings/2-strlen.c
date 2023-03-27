64ab1aa5c670aaead200064eed28527173ba3dad
Mohamed Ali <mohamed.elnakeb24@gmail.com>
2023-03-27 22:01:13 +0200

    commit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -7,10 +7,12 @@
  *
  * Return: The length of the string.
  */
-int _strlen(char *s) {
+int _strlen(char *s)
+{
 	int len = 0;
 
-	while (*s != '\0') {
+	while (*s != '\0')
+	{
 		len++;
 		s++;
 	}
